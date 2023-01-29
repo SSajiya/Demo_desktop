5ade2ea8994f4039347a4e015ec20830cd28360d
SSajiya <sajiyashaikh598@gmail.com>
2023-01-29 17:33:55 +0530

    Demo_Task

---
+++ First_Project/src/first_project/First_program.java
@@ -0,0 +1,11 @@
+package first_project;
+
+public class First_program {
+
+	public static void main(String[] args) {
+     System.out.println("Hello");
+     System.out.println("Everyone");
+
+	}
+
+}
---
+++ First_Project/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Asusn
+ *
+ */
+module First_Project {
+}
